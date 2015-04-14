473b5772a971feeb3cef10bd9e7070430e0a2181
Paul Osborn <posborn@wescom.org>
2015-04-14 11:00:31 -0600

    Sexy up the settings
    -Add option to disable auto match advance
    -Make delay picker a dialogpreference
    -Dynamically set settings title & summaries.
    -Hide or unhide the delay picker based on auto advance

--- java/com/raidzero/lolstats/activities/SettingsActivity.java
+++ java/com/raidzero/lolstats/activities/SettingsActivity.java
@@ -1,21 +1,23 @@
 package com.raidzero.lolstats.activities;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBarActivity;
 
 import com.raidzero.lolstats.R;
+import com.raidzero.lolstats.fragments.SettingsFragment;
 
 /**
  * Created by posborn on 4/13/15.
  */
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends ActionBarActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.settings);
+        setContentView(R.layout.settings);
 
+        getFragmentManager().beginTransaction().replace(R.id.frameLayout_settings,
+                new SettingsFragment()).commit();
     }
 }
--- java/com/raidzero/lolstats/data/NumberPickerPreference.java
+++ java/com/raidzero/lolstats/data/NumberPickerPreference.java
@@ -1,129 +1,51 @@
 package com.raidzero.lolstats.data;
 
+import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.res.TypedArray;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.preference.DialogPreference;
-import android.preference.Preference;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.NumberPicker;
-import android.widget.TextView;
 
 import com.raidzero.lolstats.R;
 
 /**
  * Created by posborn on 4/13/15.
  */
-public class NumberPickerPreference extends Preference {
+public class NumberPickerPreference extends DialogPreference {
     private static final String tag = "NumberPickerPreference";
 
-    private int currentValue;
+    private NumberPicker mNumPicker;
+
     public NumberPickerPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setLayoutResource(R.layout.number_picker);
+
+        setPersistent(true);
+        setDialogLayoutResource(R.layout.number_picker);
     }
 
     @Override
-    protected void onBindView(View view) {
-        super.onBindView(view);
+    public void onBindDialogView(View view) {
+        mNumPicker = (NumberPicker) view.findViewById(R.id.numPicker);
 
-        final NumberPicker numPicker = (NumberPicker) view.findViewById(R.id.numPicker);
+        // disable direct text input
+        mNumPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 
-        // disable direct text editing
-        numPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+        // min 1, max 60
+        mNumPicker.setMinValue(1);
+        mNumPicker.setMaxValue(60);
 
-        numPicker.setMinValue(1);
-        numPicker.setMaxValue(60);
+        mNumPicker.setValue(getPersistedInt(4));
 
-        numPicker.setValue(numPicker.getValue());
-
-        numPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+        // set up listener
+        mNumPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
-            public void onValueChange(NumberPicker numberPicker, int i, int i1) {
-                persistInt(numberPicker.getValue());
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                persistInt(newVal);
             }
         });
+
+        super.onBindDialogView(view);
     }
-
-    @Override
-    protected Object onGetDefaultValue(TypedArray a, int index) {
-        return a.getInt(index, 4);
-    }
-
-    @Override
-    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-        if (restoreValue) {
-            currentValue = getPersistedInt(4);
-        } else {
-            currentValue = (int) defaultValue;
-            persistInt(currentValue);
-        }
-    }
-
-    @Override
-    protected Parcelable onSaveInstanceState() {
-        final Parcelable superState = super.onSaveInstanceState();
-
-        if (isPersistent()) {
-            return superState;
-        }
-
-        final SavedState myState = new SavedState(superState);
-        myState.currentValue = currentValue;
-
-        return myState;
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Parcelable state) {
-        if (!state.getClass().equals(SavedState.class)) {
-            super.onRestoreInstanceState(state);
-            return;
-        }
-
-        SavedState myState = (SavedState) state;
-        super.onRestoreInstanceState(myState.getSuperState());
-
-        currentValue = myState.currentValue;
-        notifyChanged();
-    }
-
-    private static class SavedState extends BaseSavedState {
-
-        int currentValue;
-
-        private SavedState(Parcel source) {
-            super(source);
-
-            currentValue = source.readInt();
-        }
-
-        @Override
-        public void writeToParcel(Parcel dest, int flags) {
-            super.writeToParcel(dest, flags);
-
-            dest.writeInt(currentValue);
-        }
-
-        public SavedState(Parcelable superState) {
-            super(superState);
-        }
-
-        public static final Creator<SavedState> CREATOR =
-                new Creator<SavedState>() {
-                    @Override
-                    public SavedState createFromParcel(Parcel source) {
-                        return new SavedState(source);
-                    }
-
-                    @Override
-                    public SavedState[] newArray(int size) {
-                        return new SavedState[size];
-                    }
-                };
-    }
-
 }
